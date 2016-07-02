42285181326e5c1e4b40bc3db0036d2aab662afd
Tylor Shin <shincode@icloud.com>
2016-07-02 17:00:52 +0900

    Added navbar

--- imports/navbar/navbar.jsx
+++ imports/navbar/navbar.jsx
@@ -6,20 +6,25 @@ const Navbar = ({ currentUser }) => {
   let rightMenu;
   if (currentUser.get('user').isEmpty()) {
     rightMenu = (
-      <Link to="signin">로그인</Link>
+      <Link to="signin">
+        <i className="fa fa-key fa-lg" />
+      </Link>
     );
   } else {
     rightMenu = (
-      <span>서랍</span>
+      <span>
+        <i className="fa fa-th-large fa-lg" />
+      </span>
     );
   }
   return (
     <div className="navbar-component">
       <div className="navbar-leftmenu-wrapper">
-        User
+        <i className="fa fa-user fa-lg" />
       </div>
       <div className="navbar-logo">
-        Strings
+        <i className="fa fa-long-arrow-down fa-lg" />
+        <span className="navbar-logo-title">STRINGS</span>
       </div>
       <div className="navbar-rightmenu-wrapper">
         {rightMenu}
